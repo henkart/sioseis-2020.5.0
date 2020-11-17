d1a829e884afda6d3a791d0574dfe9964bf8fdd9
Paul Henkart <phenkart@gmail.com>
2020-11-16 16:58:09 -0800

    test

---
+++ hello.f
@@ -0,0 +1,9 @@
+      PRINT *,'Hello gfortran World'
+      CALL helloc()
+      PRINT *
+      PRINT *,'Hello SIOSEIS world'
+      PRINT *
+      PRINT *,'gfortran must have been made using the same gcc'
+      PRINT *,'Mac users: DO NOT USE HOMEBREW'
+      PRINT *,'Mac users should use MacPorts for both gcc and gfortran'
+      END
