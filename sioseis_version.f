1c7e606b5a3a615abf3cdd6687115f8a4117e855
Paul Henkart <phenkart@gmail.com>
2020-11-17 15:58:02 -0800

    new version 2020.5.1

--- sioseis_version.f
+++ sioseis_version.f
@@ -2,7 +2,7 @@
       COMMON /ver/ver
       CHARACTER*80 ver
       DATA ver
-     &/' SIOSEIS ver 2020.5.0 (14 July 2020)'/
+     &/' SIOSEIS ver 2020.5.1 (17 Nov. 2020)'/
       PRINT *,ver
       RETURN
       END
